00ded2f150e2cbb42ec0e7327caf05b867838db3
Alex Blackham <ablackham@partspedigree.com>
2019-05-12 10:15:38 +0100

    add extra register alias

--- src/commands/register.js
+++ src/commands/register.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  aliases: [path.basename(__filename).split('.')[0], 'login'],
+  aliases: [path.basename(__filename).split('.')[0], 'login', 'link'],
   permissions: [],
   disabled: false,
   description: 'Allows a user to link their steam account with discord.',
