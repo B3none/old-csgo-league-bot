9a48fe1f3b0a4b29489fe1b925405050b7acef3c
Alex Blackham <ablackham@partspedigree.com>
2019-05-12 21:10:56 +0100

    implement axios and new auth system

--- src/commands/register.js
+++ src/commands/register.js
@@ -1,5 +1,8 @@
 const config = require('../../config');
 const path = require('path');
+const axios = require('axios');
+
+const http = new axios();
 
 module.exports = {
   aliases: [path.basename(__filename).split('.')[0], 'login', 'link'],
@@ -7,30 +10,34 @@ module.exports = {
   disabled: false,
   description: 'Allows a user to link their steam account with discord.',
   command: (client, message) => {
-    let author = message.author;
+    http.get(`https://league.redlinecs.net/discord/generate/${author.id}`)
+      .then(response => {
+        let { code } = response.data;
+        let author = message.author;
 
-    message.channel.send({
-      embed: {
-        color: Number(config.colour),
-        description: `<@${author.id}> Please follow the instructions sent directly to you in order to complete the registration process.`
-      }
-    });
-
-    message.author.send({
-      embed: {
-        author: {
-          icon_url: client.user.avatarURL,
-          name: `${client.user.username} Authenticator`
-        },
-        color: Number(config.colour),
-        description: `Please follow the following url in order to link your discord to our system. Please ensure you're logged in to our website with steam first.`,
-        fields: [
-          {
-            name: 'Steam login URL:',
-            value: 'https://league.redlinecs.net'
+        message.channel.send({
+          embed: {
+            color: Number(config.colour),
+            description: `<@${author.id}> Please follow the instructions sent directly to you in order to complete the registration process.`
           }
-        ]
-      }
-    });
+        });
+
+        message.author.send({
+          embed: {
+            author: {
+              icon_url: client.user.avatarURL,
+              name: `${client.user.username} Authenticator`
+            },
+            color: Number(config.colour),
+            description: `Please follow the following url in order to link your discord to our system. Please ensure you're logged in to our website with steam first.`,
+            fields: [
+              {
+                name: 'Steam login URL:',
+                value: 'https://league.redlinecs.net/discord/' + author.id + code
+              }
+            ]
+          }
+        });
+      })
   }
 };
