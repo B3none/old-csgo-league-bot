e494e3310946692f16091b8d6c147fb9f44e6ae5
B3none <ablackham2000@gmail.com>
2019-05-14 01:55:24 +0100

    remove redundant guild event listeners

--- src/index.js
+++ src/index.js
@@ -4,21 +4,18 @@ const client = new Discord.Client();
 
 const config = require('../config.json');
 
-const guildHelper = require('./helpers/guild');
+// const guildHelper = require('./helpers/guild');
 const messageHelper = require('./helpers/message');
 
 client.on('ready', () => {
-    console.log(`nerdRage's Discord Bot has been started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`);
+    console.log(`The League Discord Bot has been started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`);
     client.user.setActivity(require('../package.json').version);
 });
 
-client.on('guildCreate', guild => guildHelper.guildCreate(guild));
-
-client.on('guildDelete', guild => guildHelper.guildDelete(guild));
-
-client.on('guildMemberAdd', member => guildHelper.welcome(member));
-
-client.on('guildMemberRemove', member => guildHelper.unwelcome(member));
+// client.on('guildCreate', guild => guildHelper.guildCreate(guild));
+// client.on('guildDelete', guild => guildHelper.guildDelete(guild));
+// client.on('guildMemberAdd', member => guildHelper.welcome(member));
+// client.on('guildMemberRemove', member => guildHelper.unwelcome(member));
 
 client.on('message', message => messageHelper.message(client, config.prefix, message));
 
