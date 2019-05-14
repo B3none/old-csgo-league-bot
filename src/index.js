8d535f26d0059f8bc760904b3b459db2d331e45b
B3none <ablackham2000@gmail.com>
2019-05-14 01:56:39 +0100

    removed it altogether

--- src/index.js
+++ src/index.js
@@ -4,18 +4,12 @@ const client = new Discord.Client();
 
 const config = require('../config.json');
 
-// const guildHelper = require('./helpers/guild');
 const messageHelper = require('./helpers/message');
 
 client.on('ready', () => {
     console.log(`The League Discord Bot has been started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`);
     client.user.setActivity(require('../package.json').version);
 });
-
-// client.on('guildCreate', guild => guildHelper.guildCreate(guild));
-// client.on('guildDelete', guild => guildHelper.guildDelete(guild));
-// client.on('guildMemberAdd', member => guildHelper.welcome(member));
-// client.on('guildMemberRemove', member => guildHelper.unwelcome(member));
 
 client.on('message', message => messageHelper.message(client, config.prefix, message));
 
