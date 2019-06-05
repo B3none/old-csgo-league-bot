2f9986a2e91edd088259679c8c3d45d860d78373
⭐ B3none <ablackham2000@gmail.com>
2019-06-06 00:49:30 +0100

    continued implementing update command

--- src/commands/update.js
+++ src/commands/update.js
@@ -2,50 +2,57 @@ const config = require('../../app/config');
 const path = require('path');
 const axios = require('axios');
 
+const instance = axios.create({
+  baseURL: config.url,
+  timeout: 1000,
+  headers: {
+    'authentication': config.api_key
+  }
+});
+
 module.exports = {
   aliases: [path.basename(__filename).split('.')[0]],
   permissions: [],
-  disabled: false,
+  disabled: true,
   description: 'Allows a user to update their Discord name on the league service.',
   command: (client, message) => {
     let author = message.author;
 
-    axios.get(`${config.url}/discord/generate/${author.id}`)
+    instance.post(`/discord/update/${author.id}`, {
+      name: author.username
+    })
       .then(response => {
-        let { code,error } = response.data;
+        let { success,error } = response.data;
 
-        if (code !== null) {
-          message.channel.send({
-            embed: {
-              color: Number(config.colour),
-              description: `<@${author.id}> Please follow the instructions sent directly to you in order to complete the registration process.`
-            }
-          });
-
-          message.author.send({
+        if (!success) {
+          let message = {
             embed: {
               author: {
                 icon_url: client.user.avatarURL,
                 name: `${client.user.username} Authenticator`
               },
               color: Number(config.colour),
-              description: `Please follow the URL below to link your Discord account to your League profile. Ensure you're logged into the website with prior to linking. This link will expire in 15 minutes.`,
-              fields: [
-                {
-                  name: 'Steam login URL:',
-                  value: `${config.url}/discord/${author.id}/${code}`
-                }
-              ]
+              description: `There was an error updating your name!`,
+              fields: []
             }
-          });
-        } else {
-          message.channel.send({
-            embed: {
-              color: Number(config.colour),
-              description: `<@${author.id}> ${error} If you're really stuck then please message one of the **!authors**`
-            }
-          });
+          };
+
+          if (error === 'link_discord') {
+            message.embed.fields.push({
+              name: 'Please link your discord with our system first using the !login command.'
+            });
+          }
+
+          message.author.send(message);
+          return;
         }
+
+        message.channel.send({
+          embed: {
+            color: Number(config.colour),
+            description: `<@${author.id}> You've successfully updated your name on our system.`
+          }
+        });
       })
   }
 };
