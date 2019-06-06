e1a460581634a08f3fb290fa3df81605231665f0
⭐ B3none <ablackham2000@gmail.com>
2019-06-06 02:45:41 +0100

    altered queue joining code

--- src/commands/queuejoin.js
+++ src/commands/queuejoin.js
@@ -1,6 +1,15 @@
 const config = require('../../app/config');
 const path = require('path');
 const queueHelper = require('../helpers/queue');
+const axios = require('axios');
+
+const instance = axios.create({
+  baseURL: config.url,
+  timeout: 1000,
+  headers: {
+    'authentication': config.api_key
+  }
+});
 
 module.exports = {
     aliases: [path.basename(__filename).split('.')[0], 'qj', 'joinqueue'],
@@ -8,18 +17,35 @@ module.exports = {
     disabled: false,
     description: 'Allows a player to join the matchmaking queue',
     command: (client, message) => {
+      instance.get('/discord/check/' + message.author.id)
+        .then(response => {
+          if (!response.linked) {
+            message.channel.send({
+              embed: {
+                author: {
+                  name: client.user.username,
+                  icon_url: client.user.avatarURL
+                },
+                color: Number(config.colour),
+                description: `<@${message.author.id}> Please \`!login\` and try again.`
+              }
+            });
 
-      const queue = queueHelper.add(message.author.id);
+            return;
+          }
 
-      message.channel.send({
-        embed: {
-          author: {
-            name: client.user.username,
-            icon_url: client.user.avatarURL
-          },
-          color: Number(config.colour),
-          description: `<@${message.author.id}> just joined the \`!queue\` | (${queue.length}/10)`
-        }
-      });
+          const queue = queueHelper.add(message.author.id);
+
+          message.channel.send({
+            embed: {
+              author: {
+                name: client.user.username,
+                icon_url: client.user.avatarURL
+              },
+              color: Number(config.colour),
+              description: `<@${message.author.id}> just joined the \`!queue\` | (${queue.length}/10)`
+            }
+          });
+        });
     }
 };
