14b2477dc5e03afd3f9f7d3d225784e5411935a0
Alex Blackham <ablackham@partspedigree.com>
2019-06-15 20:10:55 +0100

    cleaned up the game.js and syntax

--- src/commands/queuejoin.js
+++ src/commands/queuejoin.js
@@ -5,32 +5,16 @@ const axiosHelper = require('../helpers/axios');
 const axios = axiosHelper.get();
 
 module.exports = {
-    aliases: [path.basename(__filename).split('.')[0], 'qj', 'jq', 'joinqueue', 'qjoin'],
-    permissions: [],
-    description: 'Allows a player to join the matchmaking queue',
-    disabled: true,
-    command: (client, message) => {
-      axios.get('/discord/check/' + message.author.id)
-        .then(response => {
-          const { linked } = response.data;
+  aliases: [path.basename(__filename).split('.')[0], 'qj', 'jq', 'joinqueue', 'qjoin'],
+  permissions: [],
+  description: 'Allows a player to join the matchmaking queue',
+  disabled: true,
+  command: (client, message) => {
+    axios.get('/discord/check/' + message.author.id)
+      .then(response => {
+        const { linked } = response.data;
 
-          if (!linked) {
-            message.channel.send({
-              embed: {
-                author: {
-                  name: client.user.username,
-                  icon_url: client.user.avatarURL
-                },
-                color: Number(config.colour),
-                description: `<@${message.author.id}> Please \`!login\` and try again.`
-              }
-            });
-
-            return;
-          }
-
-          const queue = queueHelper.add(message.author.id);
-
+        if (!linked) {
           message.channel.send({
             embed: {
               author: {
@@ -38,9 +22,25 @@ module.exports = {
                 icon_url: client.user.avatarURL
               },
               color: Number(config.colour),
-              description: `<@${message.author.id}> just joined the \`!queue\` (${queue.length} player${queue.length === 1 ? 's' : ''})`
+              description: `<@${message.author.id}> Please \`!login\` and try again.`
             }
           });
+
+          return;
+        }
+
+        const queue = queueHelper.add(message.author.id);
+
+        message.channel.send({
+          embed: {
+            author: {
+              name: client.user.username,
+              icon_url: client.user.avatarURL
+            },
+            color: Number(config.colour),
+            description: `<@${message.author.id}> just joined the \`!queue\` (${queue.length} player${queue.length === 1 ? 's' : ''})`
+          }
         });
-    }
+      });
+  }
 };
--- src/commands/queueleave.js
+++ src/commands/queueleave.js
@@ -1,35 +1,26 @@
 const config = require('../../app/config');
 const path = require('path');
 const queueHelper = require('../helpers/queue');
-const axios = require('axios');
-
-const instance = axios.create({
-  baseURL: config.url,
-  timeout: 1000,
-  headers: {
-    'authentication': config.api_key
-  }
-});
 
 module.exports = {
-    aliases: [path.basename(__filename).split('.')[0], 'ql', 'lq', 'leavequeue'],
-    permissions: [],
-    description: 'Allows a player to join the matchmaking queue',
-    disabled: true,
-    command: (client, message) => {
-      const response = queueHelper.remove(message.author.id);
+  aliases: [path.basename(__filename).split('.')[0], 'ql', 'lq', 'leavequeue'],
+  permissions: [],
+  description: 'Allows a player to join the matchmaking queue',
+  disabled: true,
+  command: (client, message) => {
+    const response = queueHelper.remove(message.author.id);
 
-      if (response.didRemove) {
-        message.channel.send({
-          embed: {
-            author: {
-              name: client.user.username,
-              icon_url: client.user.avatarURL
-            },
-            color: Number(config.colour),
-            description: `<@${message.author.id}> just left the \`!queue\` (${response.queue.length} player${response.queue.length === 1 ? 's' : ''})`
-          }
-        });
-      }
+    if (response.didRemove) {
+      message.channel.send({
+        embed: {
+          author: {
+            name: client.user.username,
+            icon_url: client.user.avatarURL
+          },
+          color: Number(config.colour),
+          description: `<@${message.author.id}> just left the \`!queue\` (${response.queue.length} player${response.queue.length === 1 ? 's' : ''})`
+        }
+      });
     }
+  }
 };
