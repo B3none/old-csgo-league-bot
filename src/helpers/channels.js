ff3fbce947ca14619f2ecfe5079c7bfad9139adb
⭐ B3none <ablackham2000@gmail.com>
2019-06-16 00:39:26 +0100

    continued refinements and alterations

--- src/helpers/channels.js
+++ src/helpers/channels.js
@@ -1,48 +1,60 @@
 const fs = require('fs');
 const game = require('./game');
-let config = require('../../app/config.json');
+const error = require('./error');
+const config = require('../../app/config.json');
+
 module.exports = {
   checkChannels: client => {
     console.log('Checking if all required channels exist.');
+
     client.guilds.map(guild => {
       if (!guild.channels.find(x => x.name === config.queuechanneltext.toString().toLowerCase())) {
-        guild.createChannel(config.queuechanneltext, { type: 'text' })
+        guild.createChannel(config.queuechanneltext, {
+          type: 'text'
+        })
           .then(() => {
-            let channelid = client.channels.find(x => x.name === config.queuechanneltext.toString().toLowerCase()).id;
-            console.log(channelid);
-            fs.writeFile(`${process.cwd()}/app/data/textchannels.json`, JSON.stringify({queueChannelID: channelid}), (err) => {if(err)throw(err)});
+            let channelId = client.channels.find(x => x.name === config.queuechanneltext.toString().toLowerCase()).id;
+            console.log(channelId);
+            fs.writeFile(`${process.cwd()}/app/data/textchannels.json`, JSON.stringify({queueChannelID: channelId}), error);
           })
           .catch(console.error);
       }
 
       if (!guild.channels.find(x => x.name === config.queuechannelvoice.toString().toLowerCase())) {
-        guild.createChannel(config.queuechannelvoice, { type: 'voice' })
+        guild.createChannel(config.queuechannelvoice, {
+          type: 'voice'
+        })
           .then(() => {
-            let channelid = client.channels.find(x => x.name === config.queuechannelvoice.toString().toLowerCase()).id;
-            fs.writeFile(`${process.cwd()}/app/data/voicechannels.json`, JSON.stringify({queueChannelID: channelid}), (err) => {if(err)throw(err)});
+            let channelId = client.channels.find(x => x.name === config.queuechannelvoice.toString().toLowerCase()).id;
+            fs.writeFile(`${process.cwd()}/app/data/voicechannels.json`, JSON.stringify({queueChannelID: channelId}), error);
           })
           .catch(console.error);
       }
 
-      if (!guild.channels.find(x => x.name === "Team 1") && !guild.channels.find(x => x.name === "Team 2")) {
-        console.log("Creating and setting up channels for each team.");
-        guild.createChannel("Team 1", { type: 'voice' })
+      if (!guild.channels.find(x => x.name === 'Team 1') && !guild.channels.find(x => x.name === 'Team 2')) {
+        console.log('Creating and setting up channels for each team.');
+
+        guild.createChannel('Team 1', {
+          type: 'voice'
+        })
           .then(() => {
             let team1 = client.channels.find(x => x.name === "Team 1");
-            guild.createChannel("Team 2", { type: 'voice' })
+            guild.createChannel('Team 2', {
+              type: 'voice'
+            })
               .then(() => {
                 let team2 = client.channels.find(x => x.name === "Team 2");
 
-                fs.writeFile(`${process.cwd()}/app/data/teamchannels.json`, JSON.stringify({team1: team1.id, team2: team2.id}), (err) => {if(err)throw(err)});
+                fs.writeFile(`${process.cwd()}/app/data/teamchannels.json`, JSON.stringify({team1: team1.id, team2: team2.id}), error);
               })
           })
           .catch(console.error);
-      } else if(guild.channels.find(x => x.name === "Team 1") && guild.channels.find(x => x.name === "Team 2")) {
-        fs.writeFile(`${process.cwd()}/app/data/teamchannels.json`, JSON.stringify({team1: client.channels.find(x => x.name === "Team 1").id, team2: client.channels.find(x => x.name === "Team 2").id}), err => {if(err)throw(err)});
+      } else if (guild.channels.find(x => x.name === 'Team 1') && guild.channels.find(x => x.name === 'Team 2')) {
+        fs.writeFile(`${process.cwd()}/app/data/teamchannels.json`, JSON.stringify({team1: client.channels.find(x => x.name === 'Team 1').id, team2: client.channels.find(x => x.name === 'Team 2').id}), error);
       }
     });
 
-    console.log("All channels has been setup.");
+    console.log('All channels have been setup.');
   },
   switchToTeamChannels: (client, team1, team2) => {
     //GET THE CHANNELS.
