96ef107d202303e829222d3d2acda128974dc31f
⭐ B3none <ablackham2000@gmail.com>
2019-06-16 15:10:56 +0100

    syntax alterations to matchmaker

--- src/helpers/matchmaker.js
+++ src/helpers/matchmaker.js
@@ -48,32 +48,41 @@ module.exports = {
   reloadQueue: (client) => {
     //Loop through all players that is in the voice channel.
     queue.reset();
-    if(voiceChannels.queueChannelId){
-      let vcMembers = client.channels.get(voiceChannels.queueChannelId).members
-      if(vcMembers){
-        vcMembers.map((member, index) => {
+    if (voiceChannels.queueChannelId) {
+      let vcMembers = client.channels.get(voiceChannels.queueChannelId).members;
+      if (vcMembers) {
+        vcMembers.map(member => {
           axios.get(`/player/discord/${member.id}`)
-          .then((response) => {
-            queue.add({id: member.id, confirmed: false, confirmable: false, name: member.user.tag, steam: response.data.steam, elo: response.data.elo});
-            //CHECK IF THERE ARE 10 peoples inside a voice channel
-            queue.get()
-              .then(players => {
-                console.log(players);
-                if (players.length === config.playerInAMatch) {
-                  console.log('Reached enough players to start a game, sending confirmation requests.');
-                  game.initialize(players, client);
-                  game.sendAwaitConfirmation(client, players);
-                }
+            .then(response => {
+              const { steam, elo } = response.data;
+
+              queue.add({
+                id: member.id,
+                confirmed: false,
+                confirmable: false,
+                name: member.user.tag,
+                steam,
+                elo
               });
-          })
-          .catch(error => {
-            console.log('Something went wrong: ', error);
-          })
-        }) 
+
+              //CHECK IF THERE ARE 10 peoples inside a voice channel
+              queue.get()
+                .then(players => {
+                  console.log(players);
+                  if (players.length === config.playerInAMatch) {
+                    console.log('Reached enough players to start a game, sending confirmation requests.');
+                    game.initialize(players, client);
+                    game.sendAwaitConfirmation(client, players);
+                  }
+                });
+            })
+            .catch(error => {
+              console.error('Something went wrong: ', error);
+            })
+        })
       }
-      console.log(); 
-      
-    }else {console.log(`Voice queueing channel doesn't exist.`)}
-    
+    } else {
+      console.log(`Voice queueing channel doesn't exist.`);
+    }
   },
 };
