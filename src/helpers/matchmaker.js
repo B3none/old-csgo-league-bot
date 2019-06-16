5a8cd5d37f440869b38852de555424e7445add74
⭐ B3none <ablackham2000@gmail.com>
2019-06-16 19:47:28 +0100

    fix oof

--- src/helpers/matchmaker.js
+++ src/helpers/matchmaker.js
@@ -2,9 +2,66 @@ const config = require('../../app/config.json');
 const queue = require('./queue.js');
 const channels = require('./channels');
 const voiceChannels = require('../../app/data/voice_channels.json');
-const matchMaker = require('./matchmaker');
 const axiosHelper = require('./axios');
 const axios = axiosHelper.get();
+
+const reloadQueue = client => {
+  //Loop through all players that are in the voice channel.
+  queue.reset();
+
+  if (voiceChannels.queueChannelId) {
+    let queueChannel = client.channels.find(channel => channel.id === voiceChannels.queueChannelId);
+    if(!queueChannel) return;
+    let queueMembers = queueChannel.members.filter(member => member.voiceChannelID == voiceChannels.queueChannelId);
+
+    if (queueMembers) {
+      console.log(queueMembers);
+
+      queueMembers.map(member => {
+        axios.get(`/player/discord/${member.id}`)
+          .then(response => {
+            const { steam, elo, discord_name } = response.data;
+
+            console.log('checking user names');
+            console.log(`${discord_name} == ${member.user.username}`);
+            if (discord_name !== member.user.username) {
+              console.log('updating discord name of: ' + member.user.username);
+              axios.post(`/discord/update/${member.id}`, {
+                discord_name: member.user.username
+              });
+            }
+
+            queue.add({
+              id: member.id,
+              confirmed: false,
+              confirmable: false,
+              name: member.user.username,
+              steam,
+              elo
+            });
+
+            //CHECK IF THERE ARE 10 peoples inside a voice channel
+            queue.get()
+              .then(players => {
+                console.log(players);
+                if (players.length === config.players_per_match) {
+                  console.log('Reached enough players to start a game, sending confirmation requests.');
+                  const game = require('./game');
+
+                  game.initialize(players, client);
+                  game.sendAwaitConfirmation(client, players);
+                }
+              });
+          })
+          .catch(error => {
+            console.log((error.response && error.response.data) || error);
+          })
+      })
+    }
+  } else {
+    console.log(`Voice queueing channel doesn't exist.`);
+  }
+};
 
 module.exports = {
   setupQueueingChannels: client => {
@@ -49,61 +106,5 @@ module.exports = {
       queue.remove(oldMember.id);
     }
   },
-  reloadQueue: client => {
-    //Loop through all players that are in the voice channel.
-    queue.reset();
-
-    if (voiceChannels.queueChannelId) {
-      let queueChannel = client.channels.find(channel => channel.id === voiceChannels.queueChannelId);
-      if(!queueChannel) return;
-      let queueMembers = queueChannel.members.filter(member => member.voiceChannelID == voiceChannels.queueChannelId);
-
-      if (queueMembers) {
-        console.log(queueMembers);
-
-        queueMembers.map(member => {
-          axios.get(`/player/discord/${member.id}`)
-            .then(response => {
-              const { steam, elo, discord_name } = response.data;
-
-              console.log('checking user names');
-              console.log(`${discord_name} == ${member.user.username}`);
-              if (discord_name !== member.user.username) {
-                console.log('updating discord name of: ' + member.user.username);
-                axios.post(`/discord/update/${member.id}`, {
-                  discord_name: member.user.username
-                });
-              }
-
-              queue.add({
-                id: member.id,
-                confirmed: false,
-                confirmable: false,
-                name: member.user.username,
-                steam,
-                elo
-              });
-
-              //CHECK IF THERE ARE 10 peoples inside a voice channel
-              queue.get()
-                .then(players => {
-                  console.log(players);
-                  if (players.length === config.players_per_match) {
-                    console.log('Reached enough players to start a game, sending confirmation requests.');
-                    const game = require('./game');
-
-                    game.initialize(players, client);
-                    game.sendAwaitConfirmation(client, players);
-                  }
-                });
-            })
-            .catch(error => {
-              console.log((error.response && error.response.data) || error);
-            })
-        })
-      }
-    } else {
-      console.log(`Voice queueing channel doesn't exist.`);
-    }
-  },
+  reloadQueue,
 };
