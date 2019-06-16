fc5ed01b3c9d779df1c9e654130e10c5f51635e6
⭐ B3none <ablackham2000@gmail.com>
2019-06-16 01:04:52 +0100

    remove redundant inspect

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,10 @@
 const gulp = require('gulp');
 const nodemon = require('gulp-nodemon');
 
-gulp.task('default', function() {
-    nodemon({
-        script: 'src/index.js',
-        watch: ['src/*/**', 'src/*'],
-        ext: 'js',
-        nodeArgs: ['--inspect']
-    });
-});
+gulp.task('default', () =>
+  nodemon({
+    script: 'src/index.js',
+    watch: ['src/*/**', 'src/*'],
+    ext: 'js'
+  })
+);
