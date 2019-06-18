b131983ac598ce6569cbea4a98f3b50927ad45d8
ThunbergOlle <olle.thunberg03@gmail.com>
2019-06-18 13:53:31 +0200

    fixed elo embed system.

--- src/commands/elo.js
+++ src/commands/elo.js
@@ -11,16 +11,20 @@ module.exports = {
     let author = message.author;
     if((message.content.length - config.prefix.length) === 3){
         axios.post(`/player/discord/${author.id}`, {
-            discord_name: author.username
+            discord_name: undefined
           })
             .then(response => {
-              const { elo } = response.data;
-              
+              const { elo, steam } = response.data;
               message.channel.send({
-                embed: {
-                  color: Number(config.colour),
-                  description: `<@${author.id}> has an elo of ${elo}`
-                }
+                  embed:{
+                    color: Number(config.colour),
+                    author: {
+                        url: ` https://league.voidrealitygaming.co.uk/profile/${steam}`,
+                        name: `${author.username}`,
+                    },
+                    description : `Elo: ${elo}`
+                  },
+
               });
             })
             .catch(o_O => {
@@ -37,35 +41,49 @@ module.exports = {
               });
         })
     }else if((message.content.length - config.prefix.length) > 3){
-        
-        let user = message.mentions.members.first();
-        axios.post(`/player/discord/${user.id}`, {
-            discord_name: author.username
-          })
-            .then(response => {
-              const { elo } = response.data;
-              
-              message.channel.send({
-                embed: {
-                  color: Number(config.colour),
-                  description: `<@${user.id}> has an elo of ${elo}`
-                }
-              });
-            })
-            .catch(o_O => {
-              message.author.send({
-                embed: {
-                  author: {
-                    icon_url: client.user.avatarURL,
-                    name: `${client.user.username} Authenticator`
-                  },
-                  color: Number(config.colour),
-                  description: `There was an error getting your elo`,
-                  fields: []
-                }
-              });
+        let arg = message.content.toString().substr((config.prefix.length + 4), message.content.length);
+        let usr;
+        client.users.map((user, index) => {
+            if(user.username === arg) usr = user;
+        });
+        if(!usr && message.mentions.members.first() ) {
+            usr = message.mentions.members.first().user;
         }
-            )}
+        if(usr) {
+            axios.post(`/player/discord/${usr.id}`, {
+                discord_name: undefined
+              })
+                .then(response => {
+                  const { elo, steam } = response.data;
+                  
+                  message.channel.send({
+                    embed: {
+                      color: Number(config.colour),
+                      author: {
+                        url: ` https://league.voidrealitygaming.co.uk/profile/${steam}`,
+                        name: `${usr.username}`,
+                      },
+                      description : `Elo: ${elo}`
+                    }
+                  });
+                })
+                .catch(o_O => {
+                  message.author.send({
+                    embed: {
+                      author: {
+                        icon_url: client.user.avatarURL,
+                        url: ` https://league.voidrealitygaming.co.uk/profile/steamid `,
+                        name: `${client.user.username}`
+                    },
+                      color: Number(config.colour),
+                      description: `There was an error getting your elo`,
+                      fields: []
+                    }
+                  });
+                }) 
+        }
+
+        }
 
   }
 };
