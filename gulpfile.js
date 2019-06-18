a1e570aef594fd92722c2939dfb08c4b323b267a
⭐ B3none <ablackham2000@gmail.com>
2019-06-18 14:03:02 +0100

    fix initial boot error

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,31 @@
 const gulp = require('gulp');
 const nodemon = require('gulp-nodemon');
+const fs = require('fs');
 
-gulp.task('default', () =>
+const dataFiles = [
+  'afk_channel.json',
+  'matches.json',
+  'queue.json',
+  'team_channels.json',
+  'text_channels.json',
+  'voice_channels.json',
+];
+
+gulp.task('default', () => {
+  dataFiles.map(dataFile => {
+    let file = './app/data/' + dataFile;
+    if (!fs.existsSync(file)) {
+      fs.writeFile(file, "{}", err => {
+        if (err) {
+          console.error(err);
+        }
+      });
+    }
+  });
+
   nodemon({
     script: 'src/index.js',
     watch: ['src/*/**', 'src/*', 'app/config.json'],
     ext: 'js'
-  })
-);
+  });
+});
